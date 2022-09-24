adb2e38587dcd61f530ef92ac20ea7b2858f46b7
timadewale1 <timadewale1@gmail.com>
2022-09-24 18:31:24 +0100

    hello

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const soundTrack = (soundState) => {
     if(soundState === 'off'){
         on.style.display = 'block';
         off.style.display = 'none';
-        soundCloud.style.color = "#08fdd8";
+        soundCloud.style.color = "#80fdd8";
         myAudio.play();
     }
     else if(soundState === 'on'){
@@ -21,12 +21,9 @@ const soundTrack = (soundState) => {
     }
 }
 
-// Play music functionality
-
 const btnBars = document.querySelector('.bars');
 const btnTimes = document.querySelector('.times');
 const SideNav = document.querySelector('.aside');
-
 
 btnBars.addEventListener('click', () => myFunc('open'));
 btnTimes.addEventListener('click', () => myFunc('close'));
@@ -38,9 +35,61 @@ const myFunc = (navCondition) => {
         btnBars.style.display = "none";
     }
     else if(navCondition === 'close'){
-            SideNav.classList.remove('show-nav');
-            btnTimes.style.display = "none";
-            btnBars.style.display = "block";
+        SideNav.classList.remove('show-nav');
+        btnTimes.style.display = "none";
+        btnBars.style.display = "block";
     }
 }
 
+$(document).ready(function (){
+    if(!$("#myCanvas").tagcanvas({
+        textColour: "aqua",
+        outlineColour: "transparent",
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.2,
+        weight: true,
+    }, "tags")){
+        $("#myCanvasContainer");
+    }
+})
+
+const nameInput = document.querySelector('.name')
+const emailInput = document.querySelector('.email')
+const subjectInput = document.querySelector('.subject')
+const textareaInput = document.querySelector('.textarea')
+
+const contactForm = document.querySelector('.contact-form')
+
+contactForm.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    validateInput()
+})
+
+const validateInput = () => {
+
+    let email = emailInput.value
+    let textarea = textareaInput.value
+
+    if (!email && !textarea) {
+        setError(emailInput.parentElement)
+        setError(textareaInput.parentElement)
+        showMessage('Plase fill in the required inputs')
+    }
+}
+
+const setError = (input) => {
+    if (input.classList.contains("success")){
+        input.classList.remove("success")
+    } else {
+        input.classList.add("error")
+        }
+}
+
+const setSuccess = (input) => {
+    if(input.classList.contains("error")){
+        input.classList.remove("error")
+    }else{
+        input.classList.add("success")
+        }
+}
